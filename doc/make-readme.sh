b627acc88f9d4de2809326bd47216029a93fdd5e
Thomas Grenfell Smith <thomathom@gmail.com>
2011-07-26 15:54:26 -0400

    bump version; don't generate README.txt symlink

--- doc/make-readme.sh
+++ doc/make-readme.sh
@@ -8,10 +8,3 @@ awk 'BEGIN{YES=1} ! /class class/ {if (YES) print} /class class/ {YES=0}' \
 	< _build/text/index.txt \
 	> ../README.rst
 
-{
-	cd ..
-	if [ ! -e README.txt ]; then
-		ln -s README.rst README.txt
-	fi
-}
-
--- version.py
+++ version.py
@@ -2,5 +2,5 @@
 
 __version__ = '0.3'
 # release is the one with 'dev' on the end
-__release__ = '0.3dev'
+__release__ = '0.3'
 

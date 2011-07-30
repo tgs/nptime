00523118d22a0307f8683890b2d23a025d94cfc8
Thomas Grenfell Smith <tgs@resc.net>
2011-07-30 10:55:38 -0400

    Add classifiers

--- setup.py
+++ setup.py
@@ -9,9 +9,17 @@ setup(
         url = 'https://github.com/tgs/nptime',
         description = 'Extends datetime.time, allowing time arithmetic',
         long_description = open('README.rst').read(),
-        license = 'LICENSE.txt',
+        license = '3-clause BSD',
         keywords = 'time datetime timedelta',
         py_modules = ['nptime'],
+
+        classifiers = [
+          'Development Status :: 4 - Beta',
+          'Programming Language :: Python',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: OS Independent',
+          ],
 
         test_suite = 'nose.collector',
         tests_require = ['Nose'],

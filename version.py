bf0e84d2a4e1a8fd292189342a28173bc289dff5
Thomas Grenfell Smith <thomathom@gmail.com>
2018-01-29 13:05:57 -0500

    Bump version for doc fix

--- version.py
+++ version.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
-__version__ = '1.0'
+# The distinction is asked for by Sphinx.  See doc/conf.py for more.
+
+# version is just X.Y
+__version__ = '1.1'
 # release is the one with 'dev' on the end
-__release__ = '1.0'
+__release__ = '1.1'
 
